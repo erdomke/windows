a82438447e7eac351d6d444a466b0ed2a112d45a
Eric Domke <erdomke@gmail.com>
2013-08-03 11:41:34 -0400

    Significant refactoring

--- bin/jquery.windows.js
+++ bin/jquery.windows.js
@@ -13,12 +13,12 @@
 var that = this,
         pluginName = 'windows',
         defaults = {
-            snapping: true,
-            snapSpeed: 500,
-            snapInterval: 1100,
-            onScroll: function(){},
-            onSnapComplete: function(){},
-            onWindowEnter: function(){}
+          snapping: true,
+          snapSpeed: 500,
+          snapInterval: 1100,
+          onScroll: function(){},
+          onSnapComplete: function(){},
+          onWindowEnter: function(){}
         },
         options = {},
         $w = $(window),
@@ -33,34 +33,23 @@ var that = this,
      */
     function windows( element, customOptions ) {
 
-        this.element = element;
-        options = options = $.extend( {}, defaults, customOptions) ;
-        this._defaults = defaults;
-        this._name = pluginName;
-        $windows.push(element);
-        var isOnScreen = $(element).isOnScreen();
-        $(element).data('onScreen', isOnScreen);
-        if(isOnScreen) options.onWindowEnter($(element));
+      this.element = element;
+      options = options = $.extend( {}, defaults, customOptions) ;
+      this._defaults = defaults;
+      this._name = pluginName;
+      $windows.push(element);
+      var isOnScreen = $(element).isOnScreen();
+      $(element).data('onScreen', isOnScreen);
+      if(isOnScreen) options.onWindowEnter($(element));
 
     }
-
+  
     /**
      * Get ratio of element's visibility on screen
      * @return {Number} ratio 0-1
      */
-    $.fn.ratioVisible = function(){
-        if(!this.isOnScreen()) return 0;
-        var curTop = this.offset().top - $w.scrollTop();
-        var curBot = curTop + this.height();
-        var screenHeight = $w.height();
-        
-        // If the entire element is on the screen return 1 (yes, I know it is not really one)
-        if (curTop >= 0 && curBot < screenHeight) return 1;
-      
-        // Calculate the real ratio
-        var dispHeight = (curTop >= 0) ? screenHeight - curTop : screenHeight;
-        if (curBot < screenHeight) dispHeight -= screenHeight - curBot;
-        return dispHeight / screenHeight;
+    $.fn.ratioVisible = function() {
+      return new WinStats(this, $w).ratioVisible();
     };
 
     /**
@@ -68,11 +57,7 @@ var that = this,
      * @return {Boolean}
      */
     $.fn.isOnScreen = function(){
-        if (!this.is(":visible")) return false;
-        var screenHeight = $w.height(),
-            curTop = this.offset().top - $w.scrollTop(),
-            curBot = curTop + this.height();
-        return (curTop < screenHeight && curBot >= 0) ? true : false;
+      return new WinStats(this, $w).isOnScreen();
     };
 
     /**
@@ -80,41 +65,104 @@ var that = this,
      * @return {jQuery el}
      */
     var _getCurrentWindow = $.fn.getCurrentWindow = function(){
-        var maxPerc = 0,
-            maxElem = $windows[0];
-        $.each($windows, function(i){
-            var perc = $(this).ratioVisible();
-            if(Math.abs(perc) > Math.abs(maxPerc)){
-                maxElem = $(this);
-                maxPerc = perc;
-            }
-        });
-        return $(maxElem);
+      var maxPerc = 0,
+          maxElem = $windows[0];
+      $.each($windows, function(i){
+        var perc = $(this).ratioVisible();
+        if(Math.abs(perc) > Math.abs(maxPerc)){
+          maxElem = $(this);
+          maxPerc = perc;
+        }
+      });
+      return $(maxElem);
     };
 
 
     // PRIVATE API ----------------------------------------------------------
 
+    function WinStats(window, viewport) {
+      var relTop = Math.round(window.offset().top - viewport.scrollTop());
+      var relBot = Math.round(relTop + window.height());
+      var viewHeight = Math.round(viewport.height());
+      var winHeight = Math.round(window.height());
+      var self = this;
+      
+      this.isOnScreen = function() {
+        return (window.is(":visible") && relTop < viewHeight && relBot > 0) ? true : false;
+      }
+      this.prevInternalFrame = function() {
+        var scrollTo = null;
+        if (winHeight > viewHeight && relTop < 0) {
+          if (relTop < -1 * viewHeight) {
+            scrollTo = -1 * viewHeight;
+          } else {
+            scrollTo = relTop;
+          }
+          scrollTo += viewport.scrollTop();
+        }
+        return scrollTo;
+      }
+      this.nextInternalFrame = function() {
+        var scrollTo = null;
+        if (winHeight > viewHeight && relBot > viewHeight) {
+          if (relBot > 2 * viewHeight) {
+            scrollTo = viewHeight;
+          } else {
+            scrollTo = relBot - viewHeight;
+          }
+          scrollTo += viewport.scrollTop();
+        }
+        return scrollTo;
+      }
+      this.ratioVisible = function() {
+        if(!self.isOnScreen()) return 0;
+        
+        // If the entire element is on the screen return 1 (yes, I know it is not really one)
+        if (relTop >= 0 && relBot <= viewHeight) return 1;
+        
+        // Calculate the real ratio
+        var dispHeight = (relTop >= 0) ? viewHeight - relTop : viewHeight;
+        if (relBot < viewHeight) dispHeight -= viewHeight - relBot;
+        return dispHeight / viewHeight;
+      }
+      this.snapPosition = function() {
+        var scrollTo = null;
+        if (window.height() < viewHeight) {
+          scrollTo = relTop - (viewHeight - window.height()) / 2;
+          if (Math.abs(scrollTo) < 2) scrollTo = null;
+        } else if (relTop > 0) {
+          scrollTo = relTop;
+        } else if (relBot <= viewHeight) {
+          scrollTo = relBot - viewHeight;
+        } 
+        if (scrollTo !== null) scrollTo += viewport.scrollTop();
+        return scrollTo;
+      }
+      this.window = function() {
+        return window;
+      }
+    }
+  
     /**
      * Window scroll event handler
      * @return null
      */
     var _onScroll = function(){
-        s = $w.scrollTop();
+      s = $w.scrollTop();
 
-        _snapWindow();
+      _snapWindow();
 
-        options.onScroll(s);
+      options.onScroll(s);
 
-        // notify on new window entering
-        $.each($windows, function(i){
-            var $this = $(this),
-                isOnScreen = $this.isOnScreen();
-            if(isOnScreen){
-                if(!$this.data('onScreen')) options.onWindowEnter($this);
-            }
-            $this.data('onScreen', isOnScreen);
-        });
+      // notify on new window entering
+      $.each($windows, function(i){
+        var $this = $(this),
+            isOnScreen = $this.isOnScreen();
+        if(isOnScreen){
+          if(!$this.data('onScreen')) options.onWindowEnter($this);
+        }
+        $this.data('onScreen', isOnScreen);
+      });
     };
 
     var _onResize = function(){
@@ -122,91 +170,116 @@ var that = this,
     };
   
     var _onKeydown = function(e) {
-      var win;
+      var scrollTo = null;
       
       switch(e.which) {
         case 34:  // page down
-          win = _nextWindow();
+          scrollTo = _nextScroll();
+          console.log('Down ' + scrollTo);
           break;
         case 33:  // page up
-          win = _prevWindow();
+          scrollTo = _prevScroll();
+          console.log('Up ' + scrollTo);
           break;
         case 32:  // space bar
           if (e.shiftKey === true) {
-            win = _prevWindow();
+            scrollTo = _prevScroll();
           } else {
-            win = _nextWindow();
+            scrollTo = _nextScroll();
           }
           break;
       }
       
-      if (win) {
-        $('html:not(:animated),body:not(:animated)').animate({scrollTop: win.offset().top }, options.snapSpeed);
+      if (scrollTo !== null) {
+        // Keep trying recursively until it works.  (Normally only one extra time should suffice)
+        var animationComplete = false;
+        var attempts = 0;
+        
+        function TryAnimation() {
+          if (!animationComplete && attempts < 10) {
+            attempts++;
+            $('html:not(:animated),body:not(:animated)').stop(true).animate({scrollTop: scrollTo }, options.snapSpeed, function () { animationComplete = true; });
+            setTimeout(TryAnimation, options.snapSpeed+10);
+          }
+        }
+        TryAnimation();
+
         e.preventDefault();
       }
     };
   
-    var _prevWindow = function() {
-      var maxOffset = -1;
-      var maxElem = null;
-      var currOffset = _getCurrentWindow().offset().top;
-      var testOffset;
-      $.each($windows, function(i){
-        testOffset = $(this).offset().top;
-        if (testOffset < currOffset && testOffset > maxOffset) {
-          maxOffset = testOffset;
-          maxElem = $(this);
+    var _prevScroll = function() {
+      var stats = new WinStats(_getCurrentWindow(), $w);
+      var scrollTo = stats.prevInternalFrame();
+      
+      if (scrollTo === null) {
+        var maxOffset = -1;
+        var maxElem = null;
+        var currOffset = stats.window().offset().top;
+        var testOffset;
+        $.each($windows, function(i){
+          testOffset = $(this).offset().top;
+          if (testOffset < currOffset && testOffset > maxOffset && $(this).is(':visible')) {
+            maxOffset = testOffset;
+            maxElem = $(this);
+          }
+        });
+        
+        if (maxElem) {
+          stats = new WinStats($(maxElem), $w);
+          scrollTo = stats.snapPosition();
         }
-      });
-      return (maxElem ? $(maxElem) : null);
+      }
+      return scrollTo;
     }
   
-    var _nextWindow = function() {
-      var minOffset = 999999;
-      var minElem = null;
-      var currOffset = _getCurrentWindow().offset().top;
-      var testOffset;
-      $.each($windows, function(i){
-        testOffset = $(this).offset().top;
-        if (testOffset > currOffset && testOffset < minOffset) {
-          minOffset = testOffset;
-          minElem = $(this);
+    var _nextScroll = function() {
+      var stats = new WinStats(_getCurrentWindow(), $w);
+      var scrollTo = stats.nextInternalFrame();
+      
+      if (scrollTo === null) {
+        var minOffset = 999999;
+        var minElem = null;
+        var currOffset = _getCurrentWindow().offset().top;
+        var testOffset;
+        $.each($windows, function(i){
+          testOffset = $(this).offset().top;
+          if (testOffset > currOffset && testOffset < minOffset && $(this).is(':visible')) {
+            minOffset = testOffset;
+            minElem = $(this);
+          }
+        });
+        
+        if (minElem) {
+          stats = new WinStats($(minElem), $w);
+          scrollTo = stats.snapPosition();
         }
-      });
-      return (minElem ? $(minElem) : null);
+      }
+      return scrollTo;
     }
 
     var _snapWindow = function(){
-        // clear timeout if exists
-        if(t){clearTimeout(t);}
-        // check for when user has stopped scrolling, & do stuff
-        if(options.snapping){
-            var $visibleWindow = _getCurrentWindow(), // visible window
-                curTop = $visibleWindow.offset().top,
-                curBot = curTop + $visibleWindow.height(),
-                scrollTo = 0;
+      // clear timeout if exists
+      if(t){clearTimeout(t);}
+      // check for when user has stopped scrolling, & do stuff
+      if(options.snapping){
+        var stats = new WinStats(_getCurrentWindow(), $w); 
+        var scrollTo = stats.snapPosition();
+        if (scrollTo === null) return;
             
-            if (curTop > $w.scrollTop()) {
-              scrollTo = curTop;
-            } else if (curBot < ($w.scrollTop() + $w.height())) {
-              scrollTo = curBot - $w.height();
-            } else {
-              return;
+        t = setTimeout(function(){
+          var completeCalled = false;
+          // animate to top of visible window
+          $('html:not(:animated),body:not(:animated)').animate({scrollTop: scrollTo }, options.snapSpeed, function(){
+            if(!completeCalled){
+              if(t){clearTimeout(t);}
+              t = null;
+              completeCalled = true;
+              options.onSnapComplete(stats.window());
             }
-                
-            t = setTimeout(function(){
-                var completeCalled = false;
-                // animate to top of visible window
-                $('html:not(:animated),body:not(:animated)').animate({scrollTop: scrollTo }, options.snapSpeed, function(){
-                    if(!completeCalled){
-                        if(t){clearTimeout(t);}
-                        t = null;
-                        completeCalled = true;
-                        options.onSnapComplete($visibleWindow);
-                    }
-                });
-            }, options.snapInterval);
-        }
+          });
+        }, options.snapInterval);
+      }
     };
 
 
@@ -218,16 +291,16 @@ var that = this,
      */
     $.fn[pluginName] = function ( options ) {
 
-        $w.scroll(_onScroll);
-        $w.resize(_onResize);
-        $w.keydown(_onKeydown);
+      $w.scroll(_onScroll);
+      $w.resize(_onResize);
+      $w.keydown(_onKeydown);
 
-        return this.each(function(i) {
-            if (!$.data(this, 'plugin_' + pluginName)) {
-                $.data(this, 'plugin_' + pluginName,
-                new windows( this, options ));
-            }
-        });
+      return this.each(function(i) {
+        if (!$.data(this, 'plugin_' + pluginName)) {
+          $.data(this, 'plugin_' + pluginName,
+          new windows( this, options ));
+        }
+      });
     };
 
 })( jQuery, window, document );

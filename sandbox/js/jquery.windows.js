781c29be121cb5b0e2f874753d8677b486c455f1
Eric Domke <erdomke@gmail.com>
2013-08-17 14:06:33 -0400

    Extending the scroll target calculation.

--- sandbox/js/jquery.windows.js
+++ sandbox/js/jquery.windows.js
@@ -13,36 +13,10 @@
 
     var that = this,
         pluginName = 'windows',
-        options = {},
+        opts = {},
         $windows = [],
         $scrollFix = null,
-        $w = $(window);
-  
-    function Win(window, viewport) {
-      /*var relTop = Math.round(window.offset().top - viewport.scrollTop());
-      var relBot = Math.round(relTop + window.height());*/
-      
-      var origTop = window.offset().top;
-      var self = this;
-      
-      /*this.isOnScreen = function() {
-        return (window.is(":visible") && relTop < viewHeight && relBot > 0) ? true : false;
-      }*/
-      
-      this.origTop = function() {
-        return origTop;
-      }
-      this.screenOffset = function() {
-        if (window.height() < viewport.height()) {
-          return (viewport.height() - window.height()) / 2;
-        } else {
-          return 0;
-        }
-      }
-      this.window = function() {
-        return window;
-      }
-    }
+        baseOffset = 0;
   
     /**
      * A really lightweight plugin wrapper around the constructor,
@@ -55,64 +29,198 @@
         
       }
       
+      opts = $.extend( {}, $.fn[pluginName].defaults, options) ;
       var result = this.each(function(i) {
-        $windows.push(new Win($(this), $w));
+        $windows.push($(this));
       });
       
       $windows.sort(function (a, b) {
-        return a.origTop() - b.origTop();
+        return a.offset().top - b.offset().top;
       });
-      var baseIndex = 1000;
+      var baseIndex = opts.zIndex;
       for (var i = 0; i < $windows.length; i++) {
-        $windows[i].window().css('z-index', baseIndex);
-        baseIndex--;
+        $windows[i].css('z-index', baseIndex--);
       }
       $scrollFix = $('<div style="position:relative;z-index:-1"></div>').appendTo('body');
       
       return result;
     };
   
-    var baseOffset = 0;
-  
-    function getScrollTarget() {
-      var start = 0;
-      var i;
-      while (i < $windows.length) {
-      }
+    $.fn[pluginName].defaults = {
+      snapping: true,
+      scrollSpeed: 500,
+      scrollDelay: 1100,
+      enableKeys: true,
+      easing: 'swing',
+      transition: 'curtain',
+      zIndex: 1000,
+      onViewChangeStart: function () {},
+      onViewChangeEnd: function () {}
     }
   
+    function screenOffset(win, view) {
+      return (win.height() < view.height() ? (view.height() - win.height()) / 2 : 0);
+    }
   
-    function setCss() {
-      var fixedHeight = 0;
-      for (var i = 0; i < $windows.length; i++) {
-        if (($w.scrollTop() >= ($windows[i].origTop() - $windows[i].screenOffset()) && i < ($windows.length - 1)) || fixedHeight > 0) {
-          $windows[i].window().css('position', 'relative');
-          $windows[i].window().css('top', 0);
-        } else {
-          $windows[i].window().css('position', 'fixed');
-          $windows[i].window().css('top', $windows[i].screenOffset());
-          fixedHeight = $windows[i].window().height();
+    function getScrollTarget(scrollTop, callback) {
+      var start = 0,
+          end = baseOffset,
+          i = 0
+          isPercent = false,
+          viewHeight = $w.height(),
+          posPercent = 0,
+          result = null,
+          relPos = -1;
+      
+      while (i < $windows.length) {
+        start = end;
+        isPercent = !isPercent && $windows[i].height() > viewHeight;
+        end += isPercent ? 
+                  $windows[i].height() - viewHeight :
+                  ($windows[i].height() > viewHeight ? 
+                    viewHeight : 
+                    $windows[i].height() + screenOffset($windows[i], $w) - (i+1 < $windows.length ? screenOffset($windows[i+1], $w) : 0)) ;
+        posPercent = (scrollTop - start) / (end - start);
+        if (posPercent >= 0 && posPercent < 1) {
+          result = isPercent ?
+                      { win : $windows[i], snap : scrollTop, percent : posPercent, pos : i } : 
+                      (posPercent >= 0.5 ?
+                        { win : $windows[i+1], snap : end, percent : 0, pos : i+1 } :
+                        { win : $windows[i], snap : start, percent : 0, pos : i });
+        }
+        if (!isPercent) {
+          if (result) relPos++;
+          if (callback) callback.call($windows[i], $windows[i], relPos, i);
+          i++;
         }
       }
-      $scrollFix.height(fixedHeight);
+      return result;
     }
-                        
-    $.fn[pluginName].defaults = {};
+  
+    function offsetView(info, scrollTop, offset) {
+      var dist = info.win.height() - view.height();
+      var start = scrollTop - info.percent * dist;
+      var percent = Math.min(Math.max((scrollTop + offset - start) / dist, 0), 1);
+      return percent * dist + start;
+    }
+  
+    function goToNextView() {
+      var other;
+      var result = getScrollTarget(view.scrollTop(), function(win, rel, i) {
+        if (rel === 1) other = win;
+      });
+      if (result.win.height() > view.height() && result.percent < 1) {
+        offsetView
+      }
+      console.log(result);
+      //view.scrollToPosition(result.snap, result.win);
+      return true;
+    }
+    function goToPrevView() {
+      var result = getScrollTarget(view.scrollTop());
+      console.log(result);
+      //view.scrollToPosition(result.snap, result.win);
+      return true;
+    }
+  
+    function performAnim() {
+      var fixedHeight = 0;
+      var result = getScrollTarget($w.scrollTop(), function(win, rel, i) {
+        if (rel === 1 || i === $windows.length - 1) {
+          fixedHeight += win.css({'position' : 'fixed', 'top' : screenOffset(win, $w) }).height();
+        } else {
+          win.css({'position' : 'relative', 'top' : 0 });
+        }
+      });
+      $scrollFix.height(fixedHeight);
+      if (result.snap !== view.scrollTop()) {
+        view.scrollToPositionDelay(result.snap, result.win);
+      }
+    }
     
     var _onScroll = function(){
-      setCss();
+      performAnim();
     };
 
     var _onResize = function(){
-      setCss();
+      performAnim();
     };
   
     var _onKeydown = function(e) {
+      if (opts.enableKeys) {
+        var change = false;
       
+        switch(e.which) {
+          case 40:  // down arrow
+          case 34:  // page down
+            change = goToNextView();
+            break;
+          case 38:  // up arrow
+          case 33:  // page up
+            change = goToPrevView();
+            break;
+          case 32:  // space bar
+            if (e.altKey === true || e.controlKey === true) return;
+            if (e.shiftKey === true) {
+              change = goToPrevView();
+            } else {
+              change = goToNextView();
+            }
+            break;
+        }
+        
+        if (change) e.preventDefault();
+      }
     };
   
-    $w.scroll(_onScroll);
-    $w.resize(_onResize);
-    $w.keydown(_onKeydown);
+    var $w = $(window);
+    var view = new (function (view) {
+      var isAnimating = false;
+      var newScrollTop;
+      var lastWin = null;
+      var self = this;
+      var timeout = null;
+      
+      this.height = function () {
+        return view.height();
+      }
+      this.lastWindow = function () {
+        return lastWin;
+      }
+      this.scrollTop = function () {
+        return (isAnimating ? newScrollTop : view.scrollTop());
+      }
+      this.scrollToPosition = function(scrollTo, win) {
+        if (scrollTo === null || (!isAnimating && view.scrollTop() === scrollTo)) return;
+        
+        if (!isAnimating || scrollTo !== newScrollTop) {
+          if (timeout) clearTimeout(timeout);
+          // Keep trying recursively until it works.  (Normally only one extra time should suffice)
+          isAnimating = true;
+          newScrollTop = scrollTo;
+          lastWin= win;
+                    
+          //var completeCalled = !_onViewChangeStart(win, newScrollTop, true);
+          $('html:not(:animated),body:not(:animated)').stop(true).animate({scrollTop: newScrollTop }, opts.scrollSpeed, opts.easing, function () { 
+            isAnimating = false;
+            /*if (!completeCalled) {
+              completeCalled = true;
+              options.onViewChangeEnd(win, _getView(win, newScrollTop));
+            }*/
+          });
+        }
+      }
+      this.scrollToPositionDelay = function (scrollTo, win) {
+        if (scrollTo === null || scrollTo === self.scrollTop() || isAnimating) return;
+        if (timeout) clearTimeout(timeout);
+        timeout = setTimeout(function() {
+          self.scrollToPosition(scrollTo, win);
+        }, opts.scrollDelay);
+      }
+      
+      view.scroll(_onScroll);
+      view.resize(_onResize);
+      view.keydown(_onKeydown);
+    })($w);
 
 })( jQuery, window, document );
